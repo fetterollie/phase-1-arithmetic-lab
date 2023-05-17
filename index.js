4e22a669c27586fe900e44e628a70c2af3dde714
Jonathan <jonathan.fetterolf@gmail.com>
2023-05-17 07:50:48 -0400

    all tests passing

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+let num1 = 2;
+let num2 = 31;
 
-// Write your code here
+let multiply = num1 * num2;
+
+let random = Math.ceil(Math.random() * 100);
+
+let num3 = 64;
+let num4 = 60;
+
+
+let mod = num3 % num4;
+
+let set = (1, 3, 2, 18, 20);
+let max = Math.max(set);
